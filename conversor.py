de4bdf35ebce7739f5b14b7dbc69f8a81a3e49e4
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2025-05-22 19:30:13 -0300

    ajustes finais

--- conversor.py
+++ conversor.py
@@ -1,21 +1,21 @@
-#CONVERSOR
+# CONVERSOR
 valor_reais = 100.0
 taxa_dolar = 5.20
 taxa_euro = 6.15
 
-#Calcula o valor convertido em dolar
+# Calcula o valor convertido em dólar
 valor_dolar = valor_reais / taxa_dolar
-#Calcula o valor convertido em euro
+# Calcula o valor convertido em euro
 valor_euro = valor_reais / taxa_euro
 
-#Mostra ao usuario a taxa do dolar
-print(f"O valor da taxa de dolar está: {taxa_dolar:.2f}")
+# Mostra ao usuário a taxa do dólar
+print(f"O valor da taxa de dólar está: {taxa_dolar:.2f}")
 
-#Mostra ao usuario a taxa do euro
+# Mostra ao usuário a taxa do euro
 print(f"O valor da taxa de euro está: {taxa_euro:.2f}")
 
-#Calcula a conversão de reais para dolar
-print(f"O valor de {valor_reais:2f} reais convertidos em dolar é de: {valor_dolar:.2f}")
+# Mostra a conversão de reais para dólar
+print(f"O valor de {valor_reais:.2f} reais convertidos em dólar é: {valor_dolar:.2f}")
 
-##Calcula a conversão de reais para euro
-print(f"O valor {valor_reais:2f} reais convertido em euro é de: {valor_euro:.2f}")
+# Mostra a conversão de reais para euro
+print(f"O valor de {valor_reais:.2f} reais convertidos em euro é: {valor_euro:.2f}")
